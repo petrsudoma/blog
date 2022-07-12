85312e78daed6d7fcfcae090245beefe58a5d5ee
Petr Šudoma <petrsudoma28@gmail.com>
2022-07-12 17:57:21 +0200

    backend:
    - auth return types

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,19 +2,20 @@ import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { User } from './dto';
+import { AccessToken } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() data: User) {
+  signup(@Body() data: User): Promise<AccessToken> {
     return this.authService.signup(data);
   }
 
   @Post('signin')
   @HttpCode(200)
-  signin(@Body() data: User) {
+  signin(@Body() data: User): Promise<AccessToken> {
     return this.authService.signin(data);
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,12 +5,13 @@ import { PrismaService } from 'src/prisma.service';
 
 import { User } from './dto';
 import { JwtService } from '@nestjs/jwt';
+import { AccessToken } from './types';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
-  async signToken(id: string, username: string) {
+  async signToken(id: string, username: string): Promise<AccessToken> {
     const token = await this.jwtService.signAsync(
       { sub: id, username },
       { secret: process.env.JWT_SECRET, expiresIn: '60m' },
@@ -19,7 +20,7 @@ export class AuthService {
     return { access_token: token };
   }
 
-  async signup(data: User) {
+  async signup(data: User): Promise<AccessToken> {
     const hash = await argon.hash(data.password);
 
     try {
@@ -40,7 +41,7 @@ export class AuthService {
     }
   }
 
-  async signin(data: User) {
+  async signin(data: User): Promise<AccessToken> {
     const user = await this.prisma.users.findFirst({
       where: { username: data.username },
     });
