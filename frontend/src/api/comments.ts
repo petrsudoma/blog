c8eb1b0803c6cc28b57e429a3ffa7be2168dcb80
Petr Šudoma <petrsudoma28@gmail.com>
2022-09-11 17:16:29 +0200

    frontend:
    - articles show number of its comments

--- frontend/src/api/comments.ts
+++ frontend/src/api/comments.ts
@@ -3,13 +3,13 @@ import Axios from 'axios';
 function postComment(data: any) {
 	const token = localStorage.getItem('access_token');
 
-	try {
-		Axios.post((process.env.REACT_APP_BACKEND_URL as string) + '/comments', data, {
-			headers: { Authorization: 'Bearer ' + token },
-		});
-	} catch (err) {
-		console.error(err);
-	}
+	return Axios.post((process.env.REACT_APP_BACKEND_URL as string) + '/comments', data, {
+		headers: { Authorization: 'Bearer ' + token },
+	});
 }
 
-export { postComment };
+function fetchComments(articleId: string) {
+	return Axios.get((process.env.REACT_APP_BACKEND_URL as string) + '/comments/' + articleId);
+}
+
+export { postComment, fetchComments };
