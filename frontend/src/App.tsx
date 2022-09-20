897343ea51959f3d212f5eeebcabe4e4d6021522
Petr Šudoma <petrsudoma28@gmail.com>
2022-09-21 00:01:20 +0200

    frontend:
    - about page is connected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,84 +1,83 @@
-import { useContext, useEffect } from "react";
-import styled from "styled-components";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { useContext, useEffect } from 'react';
+import styled from 'styled-components';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import HomePage from "./pages/HomePage";
-import ArticleDetail from "./pages/ArticleDetail";
-import Auth from "./pages/Auth";
-import Header from "./components/Header";
-import { LoginContext } from "./context/login";
-import ArticleCreate from "./pages/ArticleCreate";
-import MyArticles from "./pages/MyArticles";
-import ArticleEdit from "./pages/ArticleEdit";
+import HomePage from './pages/HomePage';
+import ArticleDetail from './pages/ArticleDetail';
+import Auth from './pages/Auth';
+import Header from './components/Header';
+import { LoginContext } from './context/login';
+import ArticleCreate from './pages/ArticleCreate';
+import MyArticles from './pages/MyArticles';
+import ArticleEdit from './pages/ArticleEdit';
+import About from './pages/About';
 
 type ProtectedProps = {
-    children: any;
+	children: any;
 };
 const Protected: React.FC<ProtectedProps> = function (props) {
-    const loginContext = useContext(LoginContext);
+	const loginContext = useContext(LoginContext);
 
-    useEffect(() => {
-        loginContext?.signIn();
-    }, [loginContext]);
+	useEffect(() => {
+		loginContext?.signIn();
+	}, [loginContext]);
 
-    if (loginContext?.signedIn) return <>{props.children}</>;
-    else return <Navigate to="/auth" />;
+	if (loginContext?.signedIn) return <>{props.children}</>;
+	else return <Navigate to='/auth' />;
 };
 
 function App() {
-    const loginContext = useContext(LoginContext);
+	const loginContext = useContext(LoginContext);
 
-    useEffect(() => {
-        loginContext?.signIn();
-    }, [loginContext]);
+	useEffect(() => {
+		loginContext?.signIn();
+	}, [loginContext]);
 
-    return (
-        <Layout>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Navigate to="/articles" />} />
-                <Route path="/articles" element={<HomePage />} />
-                <Route
-                    path="/articles/create"
-                    element={
-                        <Protected>
-                            <ArticleCreate />
-                        </Protected>
-                    }
-                />
-                <Route
-                    path="/articles/:articleId"
-                    element={<ArticleDetail />}
-                />
-                <Route
-                    path="/articles/list"
-                    element={
-                        <Protected>
-                            <MyArticles />
-                        </Protected>
-                    }
-                />
-                <Route
-                    path="/articles/edit"
-                    element={
-                        <Protected>
-                            <ArticleEdit />
-                        </Protected>
-                    }
-                />
-                <Route path="/auth" element={<Auth />} />
-            </Routes>
-        </Layout>
-    );
+	return (
+		<Layout>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Navigate to='/articles' />} />
+				<Route path='/articles' element={<HomePage />} />
+				<Route path='/about' element={<About />} />
+				<Route
+					path='/articles/create'
+					element={
+						<Protected>
+							<ArticleCreate />
+						</Protected>
+					}
+				/>
+				<Route path='/articles/:articleId' element={<ArticleDetail />} />
+				<Route
+					path='/articles/list'
+					element={
+						<Protected>
+							<MyArticles />
+						</Protected>
+					}
+				/>
+				<Route
+					path='/articles/edit'
+					element={
+						<Protected>
+							<ArticleEdit />
+						</Protected>
+					}
+				/>
+				<Route path='/auth' element={<Auth />} />
+			</Routes>
+		</Layout>
+	);
 }
 
 export default App;
 
 const Layout = styled.div`
-    width: 70%;
-    margin: 0 auto;
+	width: 70%;
+	margin: 0 auto;
 
-    @media only screen and (max-width: 400px) {
-        width: 85%;
-    }
+	@media only screen and (max-width: 400px) {
+		width: 85%;
+	}
 `;
